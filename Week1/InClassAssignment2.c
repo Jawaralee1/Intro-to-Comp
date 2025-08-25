dc2a7ba60e26af3642b7d1dc85403bbe1db55f7d
Jawaralee1 <91759991+Jawaralee1@users.noreply.github.com>
2025-08-25 16:35:19 +0000

    New file

---
+++ Week1/InClassAssignment2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){    
+
+    char userChar;
+    int userAge;
+    float userHeight;
+    bool student;
+
+    printf("Enter your age\n");
+    scanf("%d", &userAge);
+    printf("What is the first letter of your last name\n");
+    scanf("%c", &userChar);
+    
+    printf("Enter your Height\n");
+    scanf("%f", &userHeight);
+    printf("Are you a student?\n");
+    scanf("%d", &student);
+
+    
+    
+    printf("The first letter of my last name is %c\n", userChar);
+    printf("I am %d years old\n", userAge);
+    printf("I am %.02f feet tall\n", userHeight);
+    
+    if(student == true){
+        printf("I am a student\n");
+    }
+    else{
+        printf("I am not a student\n");
+    }
+
+    return 0;
+
+}
